5c13aac3af35ba3b65a0ec53db01b08bb87e6d73
Oleg Sulyanov <oleg@sulyanov.com>
2018-09-27 21:50:00 +0300

    Initial Commit

---
+++ UrbanDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UrbanDictionary
+//
+//  Created by Oleg Sulyanov on 27/09/2018.
+//  Copyright © 2018 Oleg Sulyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
