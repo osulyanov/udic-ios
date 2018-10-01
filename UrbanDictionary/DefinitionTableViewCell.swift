fd221252364e852f3ef39871864926c7e36d5bee
Oleg Sulyanov <oleg@sulyanov.com>
2018-10-01 21:40:22 +0300

    Configure TableCell

---
+++ UrbanDictionary/DefinitionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DefinitionTableViewCell.swift
+//  UrbanDictionary
+//
+//  Created by Oleg Sulyanov on 30/09/2018.
+//  Copyright © 2018 Oleg Sulyanov. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
